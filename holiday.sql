0a348260d6e799942c2f8ccab8e26bbb72f2a219
avikoenig <avik@themis-tech.io>
2023-09-19 16:34:52 +0300

    [add-follow-to-holiday-payload] sql changes

--- holiday.sql
+++ holiday.sql
@@ -79,7 +79,7 @@ CREATE TABLE `users` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `role` varchar(40) DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
